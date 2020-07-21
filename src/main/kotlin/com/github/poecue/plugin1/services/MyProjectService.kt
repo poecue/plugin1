5068cd10af79b1fac041041315015097c3b913e1
GitHub Action <action@github.com>
2020-07-21 11:29:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/poecue/plugin1/services/MyProjectService.kt
+++ src/main/kotlin/com/github/poecue/plugin1/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.poecue.plugin1.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.poecue.plugin1.MyBundle
 
 class MyProjectService(project: Project) {
 
